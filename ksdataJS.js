3cda2ff21841a18fa4211b5accc81cd888ec14c8
NagaKurella <48324358+NagaKurella@users.noreply.github.com>
2019-03-14 01:37:07 +0530

    Update ksdataJS.js

--- ksdataJS.js
+++ ksdataJS.js
@@ -29,30 +29,37 @@
     // Download the data  //// http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson
     //alert("Retrieved " + data.length + " records from the dataset!");
                 //console.log(data);
-    myConnector.getData = function(table, doneCallback) {        
-        $.ajax({
-            url: "https://data.kcmo.org/resourse/nsn9-g8a4.json",
-            type: "GET",
-            data: {
-                    "$limit" : 5000,
-                    "$$app_token" : "ji7LlDtwzlMnFnLmSPitfXVSg"
-                  }
-        }).done(function(data) {                
-                var feat1 = data;
+    myConnector.getData = function(table, doneCallback) {   
+
                 var tableData = [];
+                $.ajax({
+                    url: "https://data.kcmo.org/resource/nsn9-g8a4.json",
+                    type: "GET",
+                    data: {
+                            "$limit" : 5000,
+                            "$$app_token" : "ji7LlDtwzlMnFnLmSPitfXVSg"
+                            }
+                    }).done(function(data) {
+                            //alert("Retrieved " + data.length + " records from the dataset!");
+                            //console.log(data);                            
+                            
+                            // Iterate over the JSON object
+                            for (var i = 0, len = data.length; i < len; i++) {
+                                tableData.push({
+                                  "report_no": data[i].report_no,
+                                  "age_1": data[i].age_1,                    
+                                  "description": data[i].description
+                                });
+                                //alert(" report_no : " + data[i].report_no + " Age :" + data[i].age_1 + "; description :  " + data[i].description );
+                            }
 
-                // Iterate over the JSON object
-                for (var i = 0, len = data.length; i < len; i++) {
-                    tableData.push({
-                        "report_no": data[i].report_no,
-                        "age_1": data[i].age_1,                    
-                        "description": data[i].description
-                    });
-                }
+                            table.appendRows(tableData);
+                            doneCallback();
+                            
+                        });
 
-                table.appendRows(tableData);
-                doneCallback();
-            });
+
+
     };
 
     tableau.registerConnector(myConnector);

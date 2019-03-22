1058a5d9e1464b9a92fb43defde2a438e764c18e
NagaKurella <48324358+NagaKurella@users.noreply.github.com>
2019-03-22 15:27:47 +0530

    Update ksdataJS.js

--- ksdataJS.js
+++ ksdataJS.js
@@ -24,7 +24,16 @@
             id: "description",
             alias: "title",
             dataType: tableau.dataTypeEnum.string
-        }, {
+        },{
+            id: "dvflag_1",
+            alias: "Is_domestic_violence_offense",
+            dataType: tableau.dataTypeEnum.string
+        },{
+            id: "invl_no_1",
+            alias: "No_persons_Involved",
+            dataType: tableau.dataTypeEnum.int
+        },                   
+        {
             id: "address",
             alias: "address (where offense occurred)",
             dataType: tableau.dataTypeEnum.string
@@ -53,7 +62,7 @@
                     url: "https://data.kcmo.org/resource/nsn9-g8a4.json",
                     type: "GET",
                     data: {
-                            "$limit" : 5000,
+                            "$limit" : 10000,
                             "$$app_token" : "ji7LlDtwzlMnFnLmSPitfXVSg"
                             }
                     }).done(function(data) {
@@ -68,6 +77,8 @@
                                   "age_1": data[i].age_1,
                                   "sex_1": data[i].sex_1,                    
                                   "description": data[i].description,
+                                  "dvflag_1": data[i].dvflag_1,
+                                  "invl_no_1": data[i].invl_no_1,  
                                   "address": data[i].address,
                                   "city": data[i].city
                                 });
@@ -88,7 +99,7 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "Kansas City Data Feed"; // This will be the data source name in Tableau
+            tableau.connectionName = "Kansas City Crime Data Feed"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
     });

1a3d69512e6512d5932d793deb3a85641af3c176
NagaKurella <48324358+NagaKurella@users.noreply.github.com>
2019-06-18 00:06:14 +0530

    Add files via upload

---
+++ dialog.js
@@ -0,0 +1,272 @@
+'use strict';
+
+(function () {
+
+  $(document).ready(function () {
+    tableau.extensions.initializeDialogAsync().then(function (openPayload) {
+      buildDialog();
+    });
+  });
+
+  // We bulid the dialogue box and ensure that settings are read from the
+  // UI Namespace and the UI is updated.
+  function buildDialog() {
+
+    var worksheetName = tableau.extensions.settings.get("worksheet");
+    if (worksheetName != undefined) {
+      // We restore the look and feel settings.
+      
+
+      // We restore the plugin settings.
+      
+    }
+
+    // Populate the worksheet drop down with a list of worksheets.
+    // Generated at the time of opening the dialogue.
+    let dashboard = tableau.extensions.dashboardContent.dashboard;
+
+    $("#tableau_db_name").html("<strong>"+dashboard.name+"</strong>");
+
+    dashboard.worksheets.forEach(function (worksheet) {
+      $("#selectWorksheet").append("<option value='" + worksheet.name + "'>" + worksheet.name + "</option>");
+    });
+
+    // Add the column orders it exists
+    var column_order = tableau.extensions.settings.get("column_order");
+    if (column_order != undefined && column_order.length > 0) {
+      var column_names_array = tableau.extensions.settings.get("column_names").split("|");
+      var column_order_array = tableau.extensions.settings.get("column_order").split("|");
+      $("#sort-it ol").text("");
+      for (var i = 0; i < column_names_array.length; i++) {
+        //alert(column_names_array[i] + " : " + column_order_array[i]);
+        $("#sort-it ol").append("<li><div class='input-field'><input id='" + column_names_array[i] + "' type='text' col_num=" + column_order_array[i] + "><label for=" + column_names_array[i] + "'>" + column_names_array[i] + "</label></div></li>");
+      }
+      
+      /*$('#sort-it ol').sortable({
+        onDrop: function (item) {
+          $(item).removeClass("dragged").removeAttr("style");
+          $("body").removeClass("dragging");
+        }
+      });*/
+
+    }
+
+    // Initialise the tabs, select and attach functions to buttons.
+    $("#selectWorksheet").val(tableau.extensions.settings.get("worksheet"));
+    $('#selectWorksheet').on('change', '', function (e) {
+      columnsUpdate();
+    });
+
+    //$("#underlying").val(tableau.extensions.settings.get("underlying"));
+    //$('#underlying').on('change', '', function (e) {
+    //  columnsUpdate();
+    //});
+
+
+    //$("#max_no_records").val(tableau.extensions.settings.get("max_no_records"));
+    $("#optradio_theme").val(tableau.extensions.settings.get("optradio_theme"));
+
+    //$('select').formSelect();
+    //$('.tabs').tabs();
+
+    $('#closeButton').click(closeDialog);
+    $('#saveButton').click(saveButton);
+    $('#resetButton').click(resetButton);
+    // Initialise the tabs, select and attach functions to buttons.
+
+  }
+
+  function columnsUpdate() {
+    var worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+    var worksheetName = $("#selectWorksheet").val();
+    //var underlying = $("#underlying").val();
+    var underlying = 2; //// Summary Datas
+
+    // Get the worksheet object for the specified names.
+    var worksheet = worksheets.find(function (sheet) {
+      return sheet.name === worksheetName;
+    });
+
+    // If underlying is 1 then get Underlying, else get Summary. Note that the columns will
+    // look different if you have summary or underlying.
+    if (underlying == 1) {
+      // Note that for our purposes and to speed things up we only want 1 record.
+      worksheet.getUnderlyingDataAsync({ maxRows: 1 }).then(function (sumdata) {
+        var worksheetColumns = sumdata.columns;
+        // This blanks out the column list
+        $("#sort-it ol").text("");
+        var counter = 1;
+        worksheetColumns.forEach(function (current_value) {
+          // For each column we add a list item with an input box and label.
+          // Note that this is based on materialisecss.
+          $("#sort-it ol").append("<li><div class='input-field'><input id='" + current_value.fieldName + "' type='text' col_num=" + counter + "><label for=" + current_value.fieldName + "'>" + current_value.fieldName + "</label></div></li>");
+          counter++;
+        });
+      });
+    } else {
+      // Note that for our purposes and to speed things up we only want 1 record.
+      worksheet.getSummaryDataAsync({ maxRows: 1 }).then(function (sumdata) {
+        var worksheetColumns = sumdata.columns;
+        // This blanks out the column list
+        $("#sort-it ol").text("");
+        var counter = 1;
+        worksheetColumns.forEach(function (current_value) {
+          // For each column we add a list item with an input box and label.
+          // Note that this is based on materialisecss.
+          $("#sort-it ol").append("<li><div class='input-field'><input id='" + current_value.fieldName + "' type='text' col_num=" + counter + "><label for=" + current_value.fieldName + "'>" + current_value.fieldName + "</label></div></li>");
+          counter++;
+        });
+      });
+    }
+
+    // Sets up the sortable elements for the columns list.
+    // https://jqueryui.com/sortable/
+    $('#sort-it ol').sortable({
+      onDrop: function (item) {
+        $(item).removeClass("dragged").removeAttr("style");
+        $("body").removeClass("dragging");
+      }
+    });
+    // Sets up the sortable elements for the columns list.
+
+  }
+
+  // This function closes the dialog box without.
+  function closeDialog() {
+    alert("in closeDialog - definition ");
+    tableau.extensions.ui.closeDialog("10");
+  } // end of close button
+
+  // This function saves then settings and then closes then closes the dialogue
+  // window.
+  function saveButton() {
+
+    alert("in saveButton - definition ");
+    // Data settings
+    tableau.extensions.settings.set("worksheet", $("#selectWorksheet").val());
+    //tableau.extensions.settings.set("max_no_records", $("#max_no_records").val());
+    //tableau.extensions.settings.set("underlying", $("#underlying").val());
+
+    tableau.extensions.settings.set("optradio_theme", $("#optradio_theme").val());
+
+    // Create a string which will hold the datatable.net css options called tableClass.
+    // Also saves the individual Y and N so that we can restore the settings when you
+    // open the configuration dialogue.
+    // https://datatables.net/examples/styling/
+    var tableClass = "";
+     /* if ($("#compact").is(":checked")) {
+      tableClass += " compact";
+      tableau.extensions.settings.set("compact", "Y");
+    } else {
+      tableau.extensions.settings.set("compact", "N");
+    }
+    if ($("#hover").is(":checked")) {
+      tableClass += " hover";
+      tableau.extensions.settings.set("hover", "Y");
+    } else {
+      tableau.extensions.settings.set("hover", "N");
+    }
+    if ($("#nowrap").is(":checked")) {
+      tableau.extensions.settings.set("nowrap", "Y");
+    } else {
+      tableClass += " nowrap";
+      tableau.extensions.settings.set("nowrap", "N");
+    }
+    if ($("#order-column").is(":checked")) {
+      tableClass += " order-column";
+      tableau.extensions.settings.set("order-column", "Y");
+    } else {
+      tableau.extensions.settings.set("order-column", "N");
+    }
+    if ($("#row-border").is(":checked")) {
+      tableClass += " row-border";
+      tableau.extensions.settings.set("row-border", "Y");
+    } else {
+      tableau.extensions.settings.set("row-border", "N");
+    }
+    if ($("#stripe").is(":checked")) {
+      tableClass += " stripe";
+      tableau.extensions.settings.set("stripe", "Y");
+    } else {
+      tableau.extensions.settings.set("stripe", "N");
+    }  */
+
+    //tableau.extensions.settings.set("table-classes", tableClass);
+
+    // Saves the individual Y and N for the plugin settings so that we can restore this
+    // when you open the configuration dialogue.
+    // https://datatables.net/extensions/buttons/examples/html5/simple.html
+    /* if ($("#export-clipboard").is(":checked")) {
+      tableau.extensions.settings.set("export-clipboard", "Y");
+    } else {
+      tableau.extensions.settings.set("export-clipboard", "N");
+    }
+    if ($("#export-excel").is(":checked")) {
+      tableau.extensions.settings.set("export-excel", "Y");
+    } else {
+      tableau.extensions.settings.set("export-excel", "N");
+    }
+    if ($("#export-csv").is(":checked")) {
+      tableau.extensions.settings.set("export-csv", "Y");
+    } else {
+      tableau.extensions.settings.set("export-csv", "N");
+    }
+    if ($("#export-pdf").is(":checked")) {
+      tableau.extensions.settings.set("export-pdf", "Y");
+    } else {
+      tableau.extensions.settings.set("export-pdf", "N");
+    }
+    if ($("#export-print").is(":checked")) {
+      tableau.extensions.settings.set("export-print", "Y");
+    } else {
+      tableau.extensions.settings.set("export-print", "N");
+    } */
+
+    // This gets the column information and saves the column order and column name.
+    // For example, if you have a data source with three columns and then reorder
+    // there so that you get the third, first and then second column, you would get: 
+    // --- column_order will look like: 3|1|2
+    // --- column_name will look like: SUM(Sales)|Country|Region
+    var column_order = "";
+    var column_name = "";
+    var counter = 0;
+    $("#sort-it").find("input").each(function (column) {      
+      // This handles the column order
+      if (counter == 0) {
+        column_order = $(this).attr("col_num");
+      } else {
+        column_order = column_order + "|" + $(this).attr("col_num");
+      }
+      // This handles the column name.
+      if (counter == 0) {
+        if ($(this).val().length > 0) {
+          column_name = $(this).val();
+        } else {
+          column_name = $(this).attr("id");
+        }
+      } else {
+        if ($(this).val().length > 0) {
+          column_name = column_name + "|" + $(this).val();
+        } else {
+          column_name = column_name + "|" + $(this).attr("id");
+        }
+      }
+      counter++;
+    });
+
+    // We save the column order and column name variables in the UI Namespace.
+    tableau.extensions.settings.set("column_order", column_order);
+    tableau.extensions.settings.set("column_names", column_name);
+
+    // Call saveAsync to save the settings before calling closeDialog.
+    tableau.extensions.settings.saveAsync().then((currentSettings) => {
+      alert("in saveButton() --> tableau.extensions.settings.saveAsync() ");
+      tableau.extensions.ui.closeDialog("10");
+    });
+
+
+
+  } // end of save button
+
+
+})();
---
+++ tabulatorIndex.js
@@ -0,0 +1,246 @@
+'use strict';
+
+(function () {
+
+  // Creates a global table reference for future use.
+  let tableReference = null;
+
+  // These variables will hold a reference to the unregister Event Listener functions.
+  // https://tableau.github.io/extensions-api/docs/interfaces/dashboard.html#addeventlistener
+  let unregisterSettingsEventListener = null;
+  let unregisterFilterEventListener = null;
+  let unregisterMarkSelectionEventListener = null;
+  let unregisterParameterEventListener = null;
+
+  $(document).ready(function () {
+    // Add the configure option in the initialiseAsync to call the configure function
+    // when we invoke this action on the user interface.
+    tableau.extensions.initializeAsync({ 'configure': configure }).then(function () {
+      // calls a function to show the table. There will be plenty of logic in this one.
+      renderDataTable();
+      
+      // We add our Settings and Parameter listeners here  listener here.
+      unregisterSettingsEventListener = tableau.extensions.settings.addEventListener(tableau.TableauEventType.SettingsChanged, (settingsEvent) => {
+        renderDataTable();
+      });
+      tableau.extensions.dashboardContent.dashboard.getParametersAsync().then(function (parameters) {
+        parameters.forEach(function (p) {
+          p.addEventListener(tableau.TableauEventType.ParameterChanged, (filterEvent) => {
+            renderDataTable();
+          });
+        });
+      });
+
+    }, function () { console.log('Error while Initializing: ' + err.toString()); });
+
+  });  //// end of $(document).ready()
+
+  // Here is where the meat of the Extension is.
+  // In a nut shell, we will try to read values from Settings and have several
+  // if statements to retrieve values and populate as appropriate. This will end
+  // with a call to a datatable function.
+  function renderDataTable() {
+
+    const worksheets = tableau.extensions.dashboardContent.dashboard.worksheets;
+    
+    // Unregister Event Listeners for old Worksheet, if exists.
+    if (unregisterFilterEventListener != null) {
+      unregisterFilterEventListener();
+    }
+    if (unregisterMarkSelectionEventListener != null) {
+      unregisterMarkSelectionEventListener();
+    }
+
+    // We will try to read the worksheet from the settings, if this exists we will show
+    // the configuration screen, otherwise we will clear the table and destroy the
+    // reference.
+    var sheetName = tableau.extensions.settings.get("worksheet");
+    if (sheetName == undefined || sheetName =="" || sheetName == null) {
+      $("#configure").show();
+      $("#datatable").text("");
+      if( tableReference !== null) {
+        tableReference.destroy();
+      }
+      // Exit the function if no worksheet name is present !!!
+      return;
+    } else {
+      // If a worksheet is selected, then we hide the configuration screen.
+      $("#configure").hide();
+    }
+
+    // Use the worksheet name saved in the Settings to find and return
+    // the worksheet object.
+    var worksheet = worksheets.find(function (sheet) {
+      return sheet.name === sheetName;
+    });
+
+    // Retrieve values the other two values from the settings dialogue window.
+    //var underlying = tableau.extensions.settings.get("underlying");
+    var underlying = 2;
+    //var max_no_records = tableau.extensions.settings.get("max_no_records");
+
+    var table_themeName = tableau.extensions.settings.get("optradio_theme");
+    if(table_themeName == "theme1"){
+      $('head').append('<link rel="stylesheet" href="http://tabulator.info/css/tabulator/4.2/tabulator.min.css" type="text/css" />');
+    }else if(table_themeName == "theme2"){
+      $('head').append('<link rel="stylesheet" href="http://tabulator.info/css/tabulator/4.2/tabulator_simple.min.css" type="text/css" />');
+    }
+    else  if(table_themeName == "theme3"){
+      $('head').append('<link rel="stylesheet" href="http://tabulator.info/css/tabulator/4.2/tabulator_midnight.min.css" type="text/css" />');
+    }
+
+    // Add an event listener to the worksheet.
+    unregisterFilterEventListener = worksheet.addEventListener(tableau.TableauEventType.FilterChanged, (filterEvent) => {
+      renderDataTable();
+    });
+    unregisterMarkSelectionEventListener = worksheet.addEventListener(tableau.TableauEventType.MarkSelectionChanged, (markSelectionEvent) => {
+      renderDataTable();
+    });
+
+    // If underlying is 1 then get Underlying, else get Summary.
+    if (underlying == 1) {
+      
+
+    } else {
+      worksheet.getSummaryDataAsync({ maxRows: 0 }).then(function (sumdata) {
+        // We will loop through our column names from our settings and save these into an array
+        // We will use this later in our datatable function.   ;; maxRows: max_no_records
+        // https://tableau.github.io/extensions-api/docs/interfaces/datatable.html#columns
+        var data_col = [];
+        var column_names = tableau.extensions.settings.get("column_names").split("|");
+        for (i = 0; i < column_names.length; i++) {
+          data_col.push({ title: column_names[i], field:column_names[i] }); //,headerVertical:true,headerFilter:true
+          //data_col.push({ title: column_names[i] });
+          //alert(column_names[i]); // ok
+          //data_col.push(column_names[i]);
+          //alert(column_names[i]);
+        }
+        
+
+        // We have created an array to match the underlying data source and then
+        // looped through to populate our array with the value data set. We also added
+        // logic to read from the column names and column order from our configiration.
+        const worksheetData = sumdata.data;
+        var column_order = tableau.extensions.settings.get("column_order").split("|");
+        
+        
+        
+        var jstr = ""; var jstr2 = ""; var tableData7 ; var tableData8 = [];
+        var tableData = makeArray(sumdata.columns.length, (sumdata.totalRowCount));
+        for (var i = 0; i < (tableData.length); i++) {
+          var str1 = "";
+          for (var j = 0; j < tableData[i].length; j++) {
+            tableData[i][j] = worksheetData[i][column_order[j]-1].formattedValue;
+            //alert(tableData[i][j]);          
+           
+            str1 = str1 + " \""+column_names[j].toString() + "\" :  \"" + tableData[i][j].toString() + "\" , ";
+            //str1 = str1 + "Col_"+j.toString() +": " + tableData[i][j].toString() + "  , ";
+          }
+          jstr = jstr + " { " + str1.trim().slice(0, -1) + " }, ";
+          jstr2 =  ' { ' + str1.trim().slice(0, -1) + ' } ';
+          //alert(jstr2);
+          //JSONobj.arPoints.push( " { " + str1.trim().slice(0, -1) + " } ");
+          //var Obj1 = JSON.parse(jstr2);
+          //alert(" Column 0 value :" + Obj1.Col_0);
+          //tableData8.push( JSON.parse(jstr2) );
+        }
+
+        //alert(jstr);
+        //tableData7 = JSON.stringify(" [ " + jstr +  "] ");
+        tableData7 = JSON.parse("["+jstr.trim().slice(0, -1)+"]");
+        //alert((tableData7));
+        //alert(JSON.stringify(tableData8));
+        //var obj999 = $.parseJSON("[" + jstr.trim().slice(0, -1) + "]");
+        var obj9999 = JSON.parse("[" + jstr.trim().slice(0, -1) + "]");
+               
+        
+        //for(var m=0;m<3;m++)
+        //  { alert(tableData[0][m]); }
+
+        // Destroy the old table.
+        if (tableReference !== null) {
+          tableReference.destroy();
+          //$("#datatable").text("");
+          $("#example-table").text("");
+        }
+
+        // Read the Settings and get the single string for UI settings.
+        var tableClass = tableau.extensions.settings.get("table-classes");
+        //$("#datatable").attr('class', '')
+        $("#example-table").attr('class', '')
+        //$("#datatable").addClass(tableClass);
+
+        // Read the Settings and create an array for the Buttons.
+        var buttons = [];
+        
+        // If there are 1 or more Export options ticked, then we will add the dom: 'Bfrtip'
+        // Else leave this out.
+        if (buttons.length > 0) {
+        } 
+        else 
+        {
+          var table = new Tabulator("#example-table", {
+            height:400, // set height of table to enable virtual DOM
+            data:tableData7, //load initial data into table    JSONobj.arPoints   tableData7 JSON.stringify(tableData7)  JSON.parse(jstr)
+            autoColumns:true, 
+            //columns:data_col,
+            layout:"fitColumns", //fit columns to width of table (optional)
+            pagination:"local",
+            paginationSize:10,
+            paginationSizeSelector:[10, 20, 30, 40, 50],                              
+          });
+          //var table = new Tabulator("#example-table", {});
+          //table.setColumns(JSON.parse(data_col.title));
+          //table.setColumns(tabledata9);
+          //table.updateData(tableData);
+          //table.setData(tableData);
+          //table.updateData(JSON.parse(jstr));
+          //$("#example-table").tabulator("setData", tableData);
+            
+
+        }
+
+
+      })
+    }
+
+  }  ////////// ------------------- end of renderDataTable()
+
+  // Creates an empty 2D array. we will use this to match the the data set returned
+  // by Tableau and repopulate this with the values we want.
+  function makeArray(d1, d2) {
+    var arr = new Array(d1), i, l;
+    for(i = 0, l = d2; i < l; i++) {
+        arr[i] = new Array(d1);
+    }
+    return arr;
+  }
+
+
+
+  // This is called when you click on the Configure button.
+  function configure() {
+
+    //const popupUrl = `${window.location.origin}/datatable/dialog.html`;
+    const popupUrl = `${window.location.origin}/dialog.html`;
+
+    let input = "";
+
+    tableau.extensions.ui.displayDialogAsync(popupUrl, input, { height: 540, width: 800 }).then((closePayload) => {
+      // The close payload is returned from the popup extension via the closeDialog method.
+      $('#interval').text(closePayload);
+    }).catch((error) => {
+      // One expected error condition is when the popup is closed by the user (meaning the user
+      // clicks the 'X' in the top right of the dialog).  This can be checked for like so:
+      switch (error.errorCode) {
+        case tableau.ErrorCodes.DialogClosedByUser:
+          console.log("Dialog was closed by user");
+          break;
+        default:
+          console.error(error.message);
+      }
+    });
+  } // end of configure() --- opening configuration page i.e. dialog.html
+
+
+})();

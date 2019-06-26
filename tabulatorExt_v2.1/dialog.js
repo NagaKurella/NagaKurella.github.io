8b119172896f8ee7538e3e4e4cb9c17ae5ee68af
NagaKurella <48324358+NagaKurella@users.noreply.github.com>
2019-06-26 19:15:30 +0530

    Update dialog.js

--- tabulatorExt_v2.1/dialog.js
+++ tabulatorExt_v2.1/dialog.js
@@ -139,6 +139,8 @@
         worksheetColumns.forEach(function (current_value) {
           // For each column we add a list item with an input box and label.
           // Note that this is based on materialisecss.
+          var fieldInfo = " Name : " + current_value.fieldName + "; DataType: " + current_value.dataType +" ";
+          alert(fieldInfo);
           $("#sort-it ol").append("<li><div class='input-field'><input id='" + current_value.fieldName + "' type='text' col_num=" + counter + "><label for=" + current_value.fieldName + "'>" + current_value.fieldName + "</label></div></li>");
           counter++;
         });

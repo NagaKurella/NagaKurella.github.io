a2a8f51635d5e211ced6e1622f9552138b25e65c
NagaKurella <48324358+NagaKurella@users.noreply.github.com>
2019-06-28 19:14:52 +0530

    Update tabulatorIndex.js

--- tabulatorExt_v2.1/tabulatorIndex.js
+++ tabulatorExt_v2.1/tabulatorIndex.js
@@ -246,7 +246,7 @@
           alasql('CREATE TABLE tblSheetData(Category STRING, Manufacturer STRING, SubCategory STRING, Quantity REAL, Sales REAL)'); 
           var crt_tbl_t1 = " CREATE TABLE tblSheetData( ";
           var crt_tbl_t1_col = "";
-          for ( var x=0;x<dimCount;x++) { crt_tbl_t1_col = crt_tbl_t1_col + col_d_m_array[x] + " STRING," } 
+          for ( var x=0;x<dimCount;x++) { crt_tbl_t1_col = crt_tbl_t1_col + col_d_m_array[x] + " STRING, "; } 
           alert(crt_tbl_t1_col);
           //alasql('INSERT INTO tblSheetData(Category,Quantity) VALUES("cate1",10) ');
           //alasql('INSERT INTO tblSheetData(Category,Quantity) VALUES("cate1",20) ');
